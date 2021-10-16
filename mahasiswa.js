6a3b98382cf930cc4d38a74eebc34c0fb13138aa
iissitiaisya <iissitiaisyah68@gmail.com>
2021-10-16 14:09:53 +0700

    first commit

---
+++ mahasiswa.js
@@ -0,0 +1,31 @@
+var mahasiswa = [
+    {
+        name : "Achmad Robbani",
+        nim  : "30811273",
+        umur : "17",
+    },
+    {
+        name : "Elyna",
+        nim  : "30873748",
+        umur : "43",
+    },
+    {
+        name : "Edi situbondo",
+        nim  : "32425255",
+        umur : "10",
+    },
+    {
+        name : "Dandi",
+        nim  : "43247291",
+        umur : "23",
+    },
+    {
+        name : "Luviana",
+        nim  : "30811273",
+        umur : "17",
+    },
+];
+ 
+for(var i=0; i<mahasiswa.length; i++){
+    console.log(mahasiswa[i])
+}
